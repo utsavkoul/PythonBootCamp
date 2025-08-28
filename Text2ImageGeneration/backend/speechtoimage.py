8d2f337fbd1088c9d468cb0508e6c5059569807a
utsavkoul <koulutsav123@gmail.com>
2025-08-28 17:03:29 +0530

    Update speechtoimage.py

--- Text2ImageGeneration/backend/speechtoimage.py
+++ Text2ImageGeneration/backend/speechtoimage.py
@@ -18,7 +18,7 @@ with sr.Microphone() as source:
         print("You:", text)
     except sr.UnknownValueError:
         print("Sorry Could not understand audio")
-os.environ["NEBIUS_API_KEY"] = "eyJhbGciOiJIUzI1NiIsImtpZCI6IlV6SXJWd1h0dnprLVRvdzlLZWstc0M1akptWXBvX1VaVkxUZlpnMDRlOFUiLCJ0eXAiOiJKV1QifQ.eyJzdWIiOiJnb29nbGUtb2F1dGgyfDEwNTIyMTI3MzU4Mzg1MjYxMDA4MiIsInNjb3BlIjoib3BlbmlkIG9mZmxpbmVfYWNjZXNzIiwiaXNzIjoiYXBpX2tleV9pc3N1ZXIiLCJhdWQiOlsiaHR0cHM6Ly9uZWJpdXMtaW5mZXJlbmNlLmV1LmF1dGgwLmNvbS9hcGkvdjIvIl0sImV4cCI6MTkxMzk2ODYyOSwidXVpZCI6ImNjNGViNzRjLWIzMjctNDZjNy1hYTc2LTBiOTE0YmI4M2FhOSIsIm5hbWUiOiJpbWFnZWdlbmVyYXRpb25vcGVuYWkiLCJleHBpcmVzX2F0IjoiMjAzMC0wOC0yNlQwOTo1NzowOSswMDAwIn0.z0q_u7GPQI5UNyqWzFsmVN-E3uX7m-g_buqp5Lcej9Y"
+os.environ["NEBIUS_API_KEY"] = ""
 
 client = OpenAI(
     base_url="https://api.studio.nebius.com/v1/",
@@ -46,28 +46,3 @@ with open(f"astronaut{file_name}.png", "wb") as f:
     # image.save(f"astronaut_horse{file_name}.png")
     file_name += 1
 
-
-
-# prompt = ""
-# model = "txt2img"
-#
-# input_data ={
-#   "input_variables": {},
-#   "prompt": f"{prompt}",
-#   "stream": false,
-#   "n": 1,
-#   "temperature": 0.7,
-#   "max_tokens": 256
-# }
-#
-# result = monsterapi.generate(model, input_data)
-# print(result["output"][0])
-# img_url = result["output"][0]
-# file_name = "generated_img.jpg"
-# response = requests.get(img_url)
-# if response.status_code == 200:
-#     with open(file_name,"wb") as f:
-#         f.write(response.content)
-#     webbrowser.open(file_name)
-# else:
-#     print("File did'nt get downloaded")
